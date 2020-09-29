7b8f4c763b28678a7f6b6ddc2d36fa2ccb0dd9d2
AishwaryaVaradannanavar <71319503+AishwaryaVaradannanavar@users.noreply.github.com>
2020-09-29 16:02:29 +0530

    Create quadratic.java

---
+++ quadratic.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class quadratic
+{
+	public static void main(String [] arg)
+	{
+		double first=0,second=0,root=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		double a=sc.nextDouble();
+		double b=sc.nextDouble();
+		double c=sc.nextDouble();
+		double det=(b*b)-(4*a*c);
+		
+		if (det>0)
+		{
+			first=(-b+Math.sqrt(det))/(2*a);
+			second=(-b-Math.sqrt(det))/(2*a);
+			System.out.printf("roots are real and distinct: %.2f and %.2f",first, second);
+		}
+		else if(det==0)
+		{
+			root=-b/(2*a);
+			System.out.printf("roots are real and equal: %.2f and %.2f",root,root);
+		}
+		else if(det<0)
+			System.out.println("real roots dont exist");
+	}	
+}
